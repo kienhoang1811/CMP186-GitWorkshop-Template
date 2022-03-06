40166ab78803f1126f1e266ac2f4ffc5492fa265
kienhoang1811 <hoangtrongkien18112000hiie@gmail.com>
2022-03-06 21:39:30 +0700

    task3

--- GitWorkshopProject/SalaryCalculator/EmployeeService.cs
+++ GitWorkshopProject/SalaryCalculator/EmployeeService.cs
@@ -15,17 +15,23 @@ namespace SalaryCalculator
 
             decimal factor = 0;
 
-            if (employeeType == EmployeeType.Engineer)
+            switch (employeeType)
             {
-                factor = 1;
-            }
-            else if (employeeType == EmployeeType.SeniorEngineer || employeeType == EmployeeType.Manager)
-            {
-                factor = 1.5m;
-            }
-            else if (employeeType == EmployeeType.SeniorManager)
-            {
-                factor = 3;
+                case EmployeeType.Engineer:
+                    factor = 1;
+                    break;
+                case EmployeeType.SeniorEngineer:
+                case EmployeeType.Manager:
+                    factor = 2;
+                    break;
+                case EmployeeType.SeniorManager:
+                    factor = 3;
+                    break;
+                case EmployeeType.Expert:
+                    factor = 3;
+                    break;
+                default:
+                    break;
             }
             // TODO: if employee is Expert, set factor = 3
 
--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -8,7 +8,8 @@ namespace SalaryCalculator
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
-
+            var Expert = EmployeeService.CalculateSalary(Expert.Manager, 9);
+            Console.WriteLine(string.Format("Expert's Salary: {0}", ExpertSalary));
             Console.ReadLine();
         }
     }
